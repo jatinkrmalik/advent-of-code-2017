27e5789f032180c37087857dd68cfcbffb9b4d0d
Jatin K Malik <jk.malik@yahoo.com>
2017-12-05 05:45:21 +0000

    Restructing the skelton of the problem solutions

--- day1/inverse-captcha.py
+++ day1/inverse-captcha.py
@@ -20,12 +20,31 @@ def inverse_captcha(num):
     
     return inv_captcha
 
+
+def inverse_captcha_halfway(num):
+    numList = [int(x) for x in str(num)] # to easily iterate over digits by splitting into a list
+    inv_captcha = 0
+    ln = len(numList)
+    half_len = ln//2
+    for i in range(ln):
+        num_half_ahead = i + half_len
+        if num_half_ahead >= ln: 
+            num_half_ahead = num_half_ahead % ln # to normalise the rotation ahead the length of the list
+        
+        if numList[i] == numList[num_half_ahead]:
+            inv_captcha += numList[i]
+        
+        else:
+            pass
+
+    return inv_captcha    
+
 def main():
     num = int(input())
     
-    result = inverse_captcha(num)
-    
-    print(result)
+    result1 = inverse_captcha(num)
+    result2 = inverse_captcha_halfway(num)
+    print('Answer of part 1 is', result1, '\nAnswer of part 2 is', result2)
 
 if __name__ == "__main__": # execute only if run as a script
     main()
