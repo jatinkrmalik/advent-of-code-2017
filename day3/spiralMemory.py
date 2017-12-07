aed41baf61736a80eff2bb4fdc414747a65ffca9
Jatin K Malik <jk.malik@yahoo.com>
2017-12-07 06:06:44 +0000

    Fixing the bug and changing logic for Day 3 part 1 problem

--- day3/spiralMemory.py
+++ day3/spiralMemory.py
@@ -1,28 +1,45 @@
-
-import math
 import sys
+import math
 
+def get_square(ip):
+    '''
+    To calculate the size of square required
+    '''
+    square = 1
+    
+    while square * square < ip:
+        square = square + 2
+    
+    return square
 
-def stats(val):
-  if val <= 1: return (0, 0, 0)
-  root = int(math.ceil(math.sqrt(val)))
-  root += 1 if (root % 2 == 0) else 0
-  max = root if root % 2 == 0 else root - 1
-  return (root**2, max, max // 2)
-
-def distanceToOne(stats, entry):
-  diff = (stats[0] - entry) % stats[2]
-  if diff > stats[2]:
-    print(stats[2] + (diff - stats[2]))
-  else:
-    print(stats[1] - diff)
+def distance_to_one(num, base, offset, side):
+    '''
+    To find the number of step after each step recursively
+    '''
+    
+    first = base + 1
+    
+    if num < first + side:
+        return int(math.fabs(num - first - offset))
+    
+    elif num < first + side * 2:
+        return int(math.fabs(num - first - offset - side))
+    
+    elif num < first + side * 3:
+        return int(math.fabs(num - first - offset - side * 2))
+    
+    else:
+        return int(math.fabs(num - first - offset - side * 3))
 
 def main():
-        ip = int(input())
-        
-        ip_stat = stats(ip)
-        
-        distanceToOne(ip_stat, ip)
+    ip = int(input()) # getting input from STDIN
+    square = get_square(ip)
+    side_len = square - 1
+    path_to_one = math.floor(square/2)
+    side_offset_to_middle = path_to_one - 1
+    prev_square = square - 2
+    print(path_to_one + distance_to_one(ip, prev_square * prev_square, side_offset_to_middle, side_len))
 
 if __name__ == "__main__":
     main()
+
